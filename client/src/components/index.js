ee00be83da34d2f0bee16220abb3d0f823a9f64a
afiqshamsudin84 <129303294+afiqshamsudin84@users.noreply.github.com>
2024-02-14 22:03:00 +0800

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,13 @@
+import CustomButton from "./CustomButton";
+import FilePicker from "./FilePicker";
+import Tab from "./Tab";
+import AIPicker from "./AIPicker";
+import ColorPicker from "./ColorPicker";
+
+export { 
+    CustomButton,
+    AIPicker,
+    FilePicker,
+    Tab,
+    ColorPicker,
+ };
